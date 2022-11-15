7d127951280813aaef2a2a18f93cc489a080f9ee
DannyRodriguezFSWD <dandemslay@hotmail.com>
2022-11-15 03:24:03 -0600

    Finish the apis

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // import express from 'express'
 const request = require('supertest');
-const assert = require('assert');
 const express = require('express') //module.exports
 const employees = require('./src/data/employees.json') 
 const app = express();
@@ -11,80 +10,72 @@ app.use(express.json());
 /* Routes */
 
 // /api/employees
-app.get('/api/employees', (req, res, next) => {    
-    let employeesData = []
-    for (let employee of employees) {        
-        employeesData.push(employee)       
-    } return res.json(employeesData)
-    //resultEmployees.push(employeesData)
-    //return 
-    console.log(employeesData)
-    
-})
-
-// /api/employees?page=1
-// /api/employees?page=2
-// /api/employees?page=N
-// app.get('/api/employees', (req, res, next) => {
-//     const page = parseInt(req.query.page)
-//     const limit = 2
-
-//     const startIndex = (page - 1) * limit
-//     const endIndex = page * limit
-
-//     const resultEmployees = employees.slice(startIndex, endIndex)
-
-//     res.json(resultEmployees)
-// })
-
-
-// /api/employees?oldest
 app.get('/api/employees', (req, res, next) => {
-    let oldest = req.query.oldest   
-    let resultEmployees = []
-    let old = 0
+    const page = parseInt(req.query.page)
+    const oldest = req.query.oldest
+    const user = req.query.user
+    const badges = req.query.badges
 
-   
-    for (let employee of employees) {        
-        if (oldest < employee.age) {
-            oldest = employee.age
-            resultEmployees = employee               
-        }      
-    }
-   
-    return res.json(resultEmployees)    
-})
-// /api/employees?user=true
-app.get('/api/employees', (req, res, next) => {
-    let user = req.query.user
-    let privileges = "user"
-    let resultEmployees = []
+    if(page){
+        // /api/employees?page=N
+        const perPage = 2
+        const from = (page - 1) * perPage
+        const to = from + page
+        res.json(employees.slice(from, to))
 
-    if (user = true ){
+    } else if(oldest == ""){
+        // /api/employees?oldest
+        let oldEmployee = {}
+        let old = 0
         for (let employee of employees) {        
-            if (privileges == employee.privileges) {                 
-                resultEmployees.push(employee)                
+            if (old < employee.age) {
+                old = employee.age
+                oldEmployee = employee               
             }      
+        }          
+        return res.json(oldEmployee)
+        
+    }  else if(user) {
+        // /api/employees?user=true
+        let privileges = "user"
+        let userEmployees = []
+        if(user == "true"){
+            for (let employee of employees) {        
+                if (privileges == employee.privileges) {                 
+                    userEmployees.push(employee)                
+                }      
+            }
         }
-    }   
-    return res.json(resultEmployees)    
+        return res.json(userEmployees)  
+    }  else if(badges){
+        // /api/employees?badges=black
+        let employeesBadges = []       
+
+        for (let employee of employees) {        
+            for (let badge of employee.badges){
+                if (badge == badges) {                 
+                    employeesBadges.push(employee)                
+                }            
+            }           
+        }       
+        return res.json(employeesBadges)    
+    
+    }  else {
+        res.json(employees)
+    }  
 })
 
 /*
-
 POST  http://localhost:8000/api/employees
 Añadirá un elemento al listado en memoria del programa (se perderá cada vez que se reinicie).
 Se validará que el body de la petición cumpla el mismo formato JSON que el resto de empleados.
 Si no cumpliera dicha validación, se devolverá status 400 con el siguiente contenido:
 {"code": "bad_request"}
 */
-app.post('/api/employees', function(req, res){
+app.post('/api/employees', function(req, res, next){
     //Check if all fields are provided and are valid:   
     if(!req.body.name ||
         !req.body.age.toString().match(/^[0-9]{2}$/g) ||        
-        !req.body.phone.home.toString().match(/^[0-9]{10}$/g) ||
-        !req.body.phone.work.toString().match(/^[0-9]{10}$/g) ||
-        !req.body.phone.ext.toString().match(/^[0-9]{3}$/g) ||
         !req.body.privileges ||
         !req.body.badges
        ){
@@ -94,31 +85,15 @@ app.post('/api/employees', function(req, res){
        employees.push({
           name: req.body.name,
           age: req.body.age,
-          phoneHome: req.body.phone.home,
-          phoneWork: req.body.phone.work,
-          phoneExt: req.body.phone.ext,
+          phone: req.body.phone,          
           privileges: req.body.privileges,
-          badges: req.body.badges
+          favorites: req.body.favorites,
+          finished: req.body.finished,
+          badges: req.body.badges,
+          points: req.body.points
        });
-       res.json({message: "New employee created.", location: "/api/employees/" + newId});
+       res.json({message: "New employee created.", location: "/api/employees/"});
     }
-})
-
-// /api/employees?badges=black
-app.get('/api/employees', (req, res, next) => {
-    let badges = req.query.badges
-    let employeesBadges = []
-    let resultEmployees = []
-
-    for (let employee of employees) {        
-        for (let badge of employee.badges){
-            if (badge == badges) {                 
-                employeesBadges.push(employee)                
-            }            
-        }           
-    }
-    resultEmployees.push(employeesBadges)
-    return res.json(resultEmployees)    
 })
 
 /*
@@ -141,19 +116,19 @@ app.get('/api/employees/:name', (req, res, next) => {
     res.status(404).send({"code": "not_found"});
 })
 
-/*
-BONUS. (Opcional). Incluir test unitarios para cada una de las rutas haciendo uso de la librería
-https://github.com/visionmedia/supertest
-*/
 
 app.listen(8000, () => {
     console.log('Ready')
 })
 
+/*
+BONUS. (Opcional). Incluir test unitarios para cada una de las rutas haciendo uso de la librería
+https://github.com/visionmedia/supertest
+*/
 request(app)
   .get('/api/employees')
   .expect('Content-Type', /json/)
-  //.expect('Content-Length', '154')
+  //.expect('Content-Length', '1574')
   .expect(200)
   .end(function(err, res) {
     if (err) throw err;
